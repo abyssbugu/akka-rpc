22f4a6848fed708b103e98b11e7440a7c4f99ef1
Abyss <547161555@qq.com>
2018-08-25 17:33:04 +0800

    master与worker之间通讯

--- src/main/scala/com/abyss/akka/rpc/Worker.scala
+++ src/main/scala/com/abyss/akka/rpc/Worker.scala
@@ -1,6 +1,6 @@
 package com.abyss.akka.rpc
 
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSelection, ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 
 /**
@@ -15,14 +15,23 @@ class Worker extends Actor {
   //prestart方法会在构造代码块执行后被调用，并且只被调用一次
   override def preStart(): Unit = {
     println("preStart method invoked")
+    //获取master actor的引用
+    //ActorContext全局变量，可以通过在已经存在的actor中，寻找目标actor
+    //调用对应actorSelection方法，
+    // 方法需要一个path路径：1、通信协议、2、master的IP地址、3、master的端口 4、创建master actor老大 5、actor层级
+    val master: ActorSelection = context.actorSelection("akka.tcp://masterActorSystem@192.168.31.127:8888/user/masterActor")
+
+    //向master发送消息
+    master ! "connect"
   }
 
   //receive方法会在prestart方法执行后被调用，表示不断的接受消息
   override def receive: Receive = {
     case "connect" => {
       println("a client connected")
-      //master发送注册成功信息给worker
-      sender ! "success"
+    }
+    case "success" =>{
+      println("注册成功")
     }
   }
 }
